33c85f53531f23a5bdc11842ea81fb89deccf6a8
JM97142 <jeanmichel.taret@gmail.com>
2023-12-10 05:25:37 +0100

    CSS modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import { Modal } from "./components";
+
 function App() {
+  const [activeModal, setActiveModal] = useState(false)
+
+  const save = (e) => {
+    e.preventDefault();
+    setActiveModal(true)
+  }
+
+  const close = (e) => {
+    setActiveModal(false)
+  }
+
+  return (
+    <div className="App">
+      <button onClick={save}>Click here</button>
+      {activeModal && (
+        <Modal message={'Employee saved !'} onClick={close} />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.css';
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App';
 
